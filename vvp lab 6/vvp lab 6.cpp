1b35e882badf32b7e5739f61fd90a925bed2913f
Валерия <valeria103020@gmail.com>
2020-11-02 15:04:00 +0300

    Добавьте файлы проекта.

---
+++ vvp lab 6/vvp lab 6.cpp
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int A, B, t;
+	scanf_s("%d %d", &A, &B);
+	t = A; //запоминаем значение А в вспомогательную переменную
+	A = B;
+	B = t;
+	printf("new A and B: %d and %d", A, B);
+	return 0;
+}
